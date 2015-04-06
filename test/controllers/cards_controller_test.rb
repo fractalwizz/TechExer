8b217805924ef4f75706e46409835d0f37389806
fractalwizz <fractalwizz@hotmail.com>
2015-04-06 16:06:48 +0000

    Testing

--- test/controllers/cards_controller_test.rb
+++ test/controllers/cards_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class CardsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
-    assert_response :success
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:cards)
   end
 
   test "should get new" do
-    get :new
-    assert_response :success
+      get :new
+      assert_response :success
   end
-
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+    test "save without title" do
+        card = Card.new
+        assert !card.save
+    end
+  
+    test "save without company" do
+        one = Card.new
+        one.first_name = "Brad"
+        one.last_name = "Pitt"
+        assert !one.save
+    end
+    
 end
